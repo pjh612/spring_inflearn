d4c0441013c4783cf0b08b7b4c2d386be2e410c3
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-08-01 15:45:44 +0900

    submit

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 	private String viewPath;
@@ -18,4 +19,13 @@ public class MyView {
 		dispatcher.forward(request,response);
 	}
 
+	public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+		modelToRequestAttribute(model, req);
+		RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+		dispatcher.forward(req,resp);
+	}
+
+	private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) {
+		model.forEach((key, value)-> req.setAttribute(key,value));
+	}
 }
--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -1,8 +1,14 @@
 package hello.servlet.web.frontcontroller.v3.controller;
 
+import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.v3.ControllerV3;
 
+import java.util.Map;
+
 public class MemberFormControllerV3 implements ControllerV3 {
-	public MemberFormControllerV3() {
+
+	@Override
+	public ModelView process(Map<String, String> paraMap) {
+		return new ModelView("new-form");
 	}
 }
