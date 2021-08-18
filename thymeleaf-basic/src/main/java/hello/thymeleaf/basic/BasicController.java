a804acc7a293fbd243c5dc6df83313f41a72bade
jinhyung <pjh_jn@naver.com>
2021-08-18 16:49:27 +0900

    20210818

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -122,6 +122,12 @@ public class BasicController {
 		model.addAttribute("users", list);
 	}
 
+	@GetMapping("/javascript")
+	public String javascript(Model model) {
+		model.addAttribute("user",new User("user\"A\"",10));
+		addUsers(model);
+		return "basic/javascript";
+	}
 
 	@Component("helloBean")
 	static class HelloBean {
