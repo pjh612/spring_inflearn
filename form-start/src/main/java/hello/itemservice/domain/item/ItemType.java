36fc55c391c1897f0b8339ad6371bcfcbd74bf63
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-08-21 00:17:40 +0900

    20210820

--- form-start/src/main/java/hello/itemservice/domain/item/ItemType.java
+++ form-start/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -3,6 +3,11 @@ package hello.itemservice.domain.item;
 
 public enum ItemType {
     BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    public String getDescription() {
+        return description;
+    }
+
     private final String description;
 
     ItemType(String description) {
